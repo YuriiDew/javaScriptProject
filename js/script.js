3180baca2be1436ddc060b876964f75f7db1f2c1
Yurii <yurakobra227@gmail.com>
2026-06-22 14:15:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//   a: 50,
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// // console.log(obj.name);
+// const arr = [1, 2, 3];
+// console.log(arr[1]);
+// // const obj = [a: 1, b: 21]
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// };
+// alert("hello");
+// const result = confirm("are you here?");
+// console.log(result);
+// const answer = +prompt("are you 18?", "18");
+// console.log(typeof answer);
+// const answers = [];
+// // answers[0] = prompt("How yours name?", "");
+// // answers[1] = prompt("How yours surname?", "");
+// // answers[2] = prompt("How yours age?", "");
+// // console.log(typeof answers);
+// "use sctrict";
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "yurii";
+// // alert(`hi,${user}`);
+
+// "use strict";
+// console.log("arr" + "- object");
+
+// let incr = 10,
+//   decr = 10;
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 2);
+// console.log(2 * 4 === 8);
+
+const isCheked = false,
+  isClose = false;
+console.log(isCheked || !isClose);
